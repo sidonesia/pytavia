e3be19141f3cd0d93bfb532bd0f96fb9abb6811b
Allen Roy Roberto <arcroberto1013@gmail.com>
2021-01-11 15:27:23 +0800

    Updated database.py -- added logic for reading table readable names, automatically add archived timestamps as well as last modified to records, updated way to create pkeys, new function to load the structure of the record as well as the foreign record structures (for 2 way relationships), and a function for getting the table readable name

--- pytavia_core/database.py
+++ pytavia_core/database.py
@@ -8,6 +8,7 @@ import config
 import random
 
 from bson.objectid import ObjectId
+from pytavia_stdlib import utils
 
 db_conn_completed = False
 
@@ -48,42 +49,103 @@ def get_db_conn(db_conn):
     return handle
 #end def
 
+def _db_add_archive_field(record):
+    record["archived_timestamp"]        = ""
+    record["archived_timestamp_str"]    = ""
+
+def db_fk_settings(record, add_archived_field = True):
+    # in models.py, we may define the reference array with a single item for the structure. We will empty it here before insert.
+    for key in record:
+        if type(record[key]) == list and key.startswith('fk'):
+            record[key] = []
+        elif add_archived_field == True and type(record[key]) == dict and key.startswith('fk'):
+            _db_add_archive_field(record[key])
+
+    record.pop('__db__readable__name__', None)      # removes the readable name of the database before insertion
+
 def get_record(db_table):
-    timestamp = int(time.time()) * 1000
-    record    = db[db_table]
+    record    = copy.deepcopy(db[db_table])
+    
+    db_fk_settings(record)
+
+    timestamp, timestamp_str = utils._get_current_timestamp()
     record["rec_timestamp"    ] = timestamp
-    record["rec_timestamp_str"] = time.strftime(
-        '%Y-%m-%d %H:%M:%S', time.localtime(int(time.time()))
-    )
+    record["rec_timestamp_str"] = timestamp_str
+
+
     record["_id" ] = ObjectId()
     record["pkey"] = str( record["_id"] )
     return copy.deepcopy( record )
 #end def
 
-def new(db_handle, db_table):
-    timestamp = int(time.time()) * 1000
-    record    = db[db_table]
+def new(db_handle, db_table, add_modified_field = True, add_archived_field = True):
+    record    = copy.deepcopy(db[db_table])
+
+    db_fk_settings(record, add_archived_field)
+
     record["__db__name__"     ] = db_table
+
+    timestamp, timestamp_str    = utils._get_current_timestamp()
     record["rec_timestamp"    ] = timestamp
-    record["rec_timestamp_str"] = time.strftime(
-        '%Y-%m-%d %H:%M:%S', time.localtime(int(time.time()))
-    )
+    record["rec_timestamp_str"] = timestamp_str
+    
+    if add_modified_field:
+        record["last_modified_timestamp"] = ""
+        record["last_modified_timestamp_str"] = ""
 
-    now_time        = int(time.time() * 1000)
-    random_int      = random.randint( config.G_RANDOM_START , config.G_RANDOM_END )
+    if add_archived_field:
+        _db_add_archive_field(record)
 
-    _id = ObjectId()
-    record["ipkey"] = str( _id )
-    record["pkey" ] = str( _id ) + "-" + str(now_time) + "-" + str(random_int)
+    random_hex   = os.urandom(24).hex()
+    random_int   = random.randint( config.G_RANDOM_START , config.G_RANDOM_END )
+    req_id       = random_hex + "-" + str(random_int)
 
+    record["_id"  ] = ObjectId()
+    record["ipkey"] = str( record["_id"] )
+    record["pkey" ] = record["ipkey"] + "-" + req_id
     mongo_record_model      = model.mongo_model( record , record , db_handle )
     return  mongo_record_model 
 #end def
 
 def load(db_handle, db_table):
-    record                  = db[db_table]
+    record    = copy.deepcopy(db[db_table])
+
+    db_fk_settings(record)
+
     record["__db__name__" ] = db_table
     mongo_record_model      = model.mongo_model( {} , record , db_handle )
     return mongo_record_model
 #end def
 #
+
+# we use this if we only need the lookup db record
+# primarily used in deep update
+def simple_load(db_table):
+    record    = copy.deepcopy(db[db_table])
+    record.pop('__db__readable__name__', None)
+
+    return record
+
+def get_fk_structure(db_table, fk_key, add_archived_field = True):
+    main_table = simple_load(db_table)
+    fk_structure = None
+
+    if fk_key in main_table:
+        fk_structure = main_table[fk_key]
+        if add_archived_field:
+            fk_structure_type = type(fk_structure)
+            if fk_structure_type == dict:
+                _db_add_archive_field(fk_structure)
+            elif fk_structure_type == list:
+                if len(fk_structure) > 0 and type(fk_structure[0]) == dict:
+                    _db_add_archive_field(fk_structure[0])
+    
+    return fk_structure
+
+def get_readable_name(db_table):
+    record = copy.deepcopy(db[db_table])
+    db_readable_name = ""
+    if "__db__readable__name__" in record:
+        db_readable_name = record["__db__readable__name__"]
+    
+    return db_readable_name
