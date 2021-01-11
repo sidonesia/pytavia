bb3c65ed7db94ceec7187e9371a1f7d0984df61b
Allen Roy Roberto <arcroberto1013@gmail.com>
2021-01-11 15:53:35 +0800

    model.py -- updated insert to insert_one. Also added examples on how to define the tables to support bulk_db_action.py operations as well as pytavia validations

--- pytavia_core/model.py
+++ pytavia_core/model.py
@@ -1,10 +1,4 @@
-import time
 import copy
-import pymongo
-import os
-import sys
-
-from bson.objectid import ObjectId
 
 class mongo_model:
 
@@ -30,12 +24,19 @@ class mongo_model:
         self._db_handle[collection_name].remove( query )
     # end def
 
-    def insert(self):
+    def insert(self, lock=None):
         collection_name = self._lookup_record["__db__name__"]
         del self._mongo_record["__db__name__"]
-        self._db_handle[collection_name].insert(  
-            self._mongo_record
-        )
+        if lock == None:
+            self._db_handle[collection_name].insert_one(  
+                self._mongo_record
+            )
+        else:
+            self._db_handle[collection_name].insert_one(  
+                self._mongo_record,
+                session=lock
+            )
+        # end if
     # end def
 
     def update(self, query):
@@ -76,5 +77,46 @@ db = {
         "pkey"                        : "",
         "misc"                        : {},
     },
+    
+"""
+### New way to define tables -- required in using bulk_db_action.py
+### Samples below
+
+    "db_book"                       : {
+        "__db__readable__name__"    : "Book",           ### define a readable table name here -- this will be used for returning meaningful status codes in pytavia validations
+        "pkey"                      : "",               
+        "name"                      : "",               ### some fields
+        "fk_publisher"              : {                 ### 1:1 Relationship -- duplicate the publisher name -- notice the pattern of fk_fieldname. db_publisher -> fk_publisher
+            "pkey"                  : "",
+            "name"                  : "",               ### foreign key field name should be exactly the same to the origin table
+        },
+        "fk_author"                 : [{                ### 1:M Relationship -- same pattern from db_author to fk_author
+            "pkey"                  : "",
+            "name"                  : "",               ### same, field names should be exactly the same for the deduplication logic to work
+            "picture"               : ""                ### duplicates the db_author's picture as well so we won't have to have a separate query
+        }]
+    },
+
+    "db_publisher"                  : {                
+        "__db__readable__name__"    : "Publisher",      ### same, define a readable name for the table -- to be displayed on response messages / descriptions
+        "pkey"                      : "",               
+        "name"                      : "",               ### duplicated in db_book
+        "fk_book"                   : [{                ### 1:M Relationship
+            "pkey"                  : "",
+            "name"                  : ""                ### duplicates db_book's name to save a query
+        }]
+    },
+
+    "db_author"                     : {                 
+        "__db__readable__name__"    : "Author",         
+        "pkey"                      : "",               
+        "name"                      : "",
+        "picture"                   : "",               ### duplicated in db_book -- notice how the field names are exactly the same
+        "fk_book"                   : [{                ### 1:M Relationship
+            "pkey"                  : "",
+            "name"                  : ""                ### again, db_book's name is duplicated here
+        }]
+    }
+"""
 
 }
